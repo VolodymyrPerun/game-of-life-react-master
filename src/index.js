afb94611b9ec7773aa1eb80aaf49e62d15ff4d5f
Volodymyr <47578369+volodymyrperun@users.noreply.github.com>
2019-09-28 14:47:25 +0300

    commit to master

--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Main extends React.Component {
+    render() {
+        constructor() {
+            super();
+            this.state = {
+                generation: 0
+            }
+        }
+        return (
+            <div>
+                <h1>The Game Of Life</h1>
+                <Grid
+                />
+                <h2>
+                    Generation: {this.state.generation}
+                </h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
 
 
